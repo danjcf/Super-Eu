4576b7fb7bd734fa7b30b52e65aac358b98b2bba
danjcf <danjcf@gmail.com>
2018-03-18 13:16:53 +0000

    Main Changes
    - Way of reading the txt file changed;
    - Minigame is functional;
    - Next is to implement Touch controls for android;

--- Assets/Resources/Scripts/MainGame/Quest.cs
+++ Assets/Resources/Scripts/MainGame/Quest.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Quest : MonoBehaviour {
 	
@@ -9,15 +11,35 @@ public class Quest : MonoBehaviour {
 	public string questDescription;
 	public string questPeriod;
 	public string questObject;
+	public bool isCompleted;
 	private QuestManager theQM;
+	//private GameObject QuestWindow;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start(){
+		theQM = GameObject.Find ("Quest Box").GetComponent<QuestManager> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void QuestTextButton()
+	{
+        theQM.QuestWindow.SetActive(true);
+        theQM.QuestWindow.transform.TransformPoint(new Vector3((2*Screen.width) / 3, Screen.height / 2, 0));
+        Text[] QuestTexts = theQM.QuestWindow.GetComponentsInChildren<Text>();
+        foreach (Text to in QuestTexts)
+        {
+            if (to.name == "MissionText")
+            {
+                to.text = questName;
+            }
+            if(to.name == "DescriptionText")
+            {
+                to.text = questDescription;
+            }
+        }
+        
+	}
+
+    public void QuestCompleted()
+	{
+		isCompleted = true;
 	}
 }
--- Assets/Resources/Scripts/MainGame/QuestManager.cs
+++ Assets/Resources/Scripts/MainGame/QuestManager.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.IO;
 
 public class QuestManager : MonoBehaviour {
 
-    //public QuestObject[] quests;
+    
 	public Quest prefab;
-	//public GameObject Quest;
-    public bool[] questCompleted;
 
+    public bool[] questCompleted;
+	private Quest[] questsList;
+	private Text QuestBox;
+	public Button[] InactiveButtons;
+	public GameObject QuestWindow;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		QuestWindow.SetActive(false);
+		QuestBox = GameObject.Find ("Quest Box").GetComponentInChildren<Text>();
 		CreateListOfQuests ();
         
 	}
@@ -25,32 +30,60 @@ public class QuestManager : MonoBehaviour {
 	}
 
 	void CreateListOfQuests(){ 																				//função que lê o ficheiro com a lista de tarefas e transforma em quests (usando o prefab da quest)
-		List<string> quests_name = ReadTextfile("Lista de Tarefas");
-		if (quests_name.Find (x => x.Contains ("Manhã")) != null) 
+		List<string> quests_name = AlternativeReadFile("Lista de Tarefas");
+		if (quests_name.Find (x => x.Contains ("Manhã")) != null) 											//Formato do ficheiro txt: Nome da quest - Descrição da quest - Duração da quest - Objecto onde vai estar a quest 
 		{
 			int morning_index = quests_name.FindIndex (x => x.Contains ("Manhã"));							//Index da palavra Manhã no txt, que representa a divisão das tarefas da manhã
-
+			int lastIndex = quests_name.Count;
 			int afternoon_index = quests_name.FindIndex (x => x.Contains ("Tarde"));
-			string[] morning_tasks = new string[afternoon_index-(morning_index+1)];
-			quests_name.CopyTo (morning_index+1,morning_tasks,0,afternoon_index-(morning_index+1));			//Retira só as tarefas do periodo da manhã
-			Text QuestBox = GameObject.Find ("Quest Box").GetComponentInChildren<Text>();
+			string[] tasks = new string[lastIndex];
+			quests_name.CopyTo (morning_index+1,tasks,0,lastIndex-1);			//Retira só as tarefas do periodo da manhã
+			int tasks_afternoonIndex = afternoon_index-1;
 			int counter = 0;
-			while(counter<morning_tasks.Length)																//Ciclo para criar as quests
+			int morningcounter = 1;
+			int afternooncounter = 1;
+			string[] foo = new string[4];
+			questsList = new Quest[tasks.Length];
+			char[] charsToTrim = {'\r'};
+			while(counter<(tasks.Length-1))														//Ciclo para criar as quests, retira-se 1 ao tasks.length por causa da string "Tarde" a meio do ficheiro
 			{
-				Quest quest = (Instantiate (prefab) as Quest);												//Cria as várias quests
-				quest.transform.SetParent (GameObject.Find ("Quest Box").transform);						//Define as quests como children do questmanager
-				quest.name = "Quest"+(counter+1);															//Insere a info do array nas quests
-				quest.questName = morning_tasks [counter];
-				quest.questNumber = counter + 1;
-				print (quest.questName + "Num.Quest:" + quest.questNumber);
-				if (quest.questNumber == 1) 
-				{
-					quest.gameObject.SetActive (true);
-					QuestBox.text = "Próxima Missão:\n" + quest.questName;
-				} 
-				else {
+				
+				if (counter < tasks_afternoonIndex && tasks [counter].Split ('\t').Length == 4) {
+					Quest quest = (Instantiate (prefab) as Quest);												//Cria as várias quests
+					quest.transform.SetParent (GameObject.Find ("Quest Box").transform);						//Define as quests como children do questmanager
+					quest.questNumber = counter + 1;
+					foo = tasks [counter].Split ('\t');													//Divide o txt através dos tabs e separa com o formato dito em cima
+					foo[3] = foo [3].Trim (charsToTrim);
+					quest.questName = foo [0];
+					quest.questDescription = foo [1];
+					quest.questPeriod = foo [2];
+					quest.questObject = foo [3];
+					quest.gameObject.name = "QuestMorning" + morningcounter;
+					quest.isCompleted = false;
 					quest.gameObject.SetActive (false);
+					questsList [counter] = quest;
+					foreach (Button bo in InactiveButtons) {
+						if (bo.name == (foo [3] + "_Button")) {                  
+							bo.onClick.AddListener (quest.QuestTextButton);
+						}
+                        bo.gameObject.SetActive (false);
+					}
+					morningcounter++;								
 				}
+
+				if(counter > tasks_afternoonIndex && tasks [counter].Split ('\t').Length == 1){					//ALTERAR PARA 4 QUANDO  O TXT TIVER COMPLETO
+					Quest quest = (Instantiate (prefab) as Quest);												//Cria as várias quests
+					quest.transform.SetParent (GameObject.Find ("Quest Box").transform);						//Define as quests como children do questmanager
+					quest.questNumber = counter + 1;
+					quest.questName = tasks [counter];
+
+					quest.gameObject.name = "QuestAfternoon" + afternooncounter;
+					quest.gameObject.SetActive (false);															//Desativa o objecto, só a primeira quest da lista fica activa
+					quest.isCompleted = false;	
+					questsList [counter] = quest;
+					afternooncounter++;
+				}
+
 				counter++;
 			}
 
@@ -60,18 +93,61 @@ public class QuestManager : MonoBehaviour {
 	}
 
 	void SetButtonCurrentQuest(){
-		Quest currentQuest = GameObject.Find ("Quest1").GetComponent<Quest>();
-		switch (currentQuest.questObject) {																//Switch que verifica qual o objecto a que o jogador se deve deslocar e activa a tarefa.
+		Quest Current = CheckCurrentQuest ();
+
+		//Quest currentQuest = GameObject.Find ("Quest1").GetComponent<Quest>();
+		print(Current.questObject);
+		switch (Current.questObject) {																//Switch que verifica qual o objecto a que o jogador se deve deslocar e activa a tarefa.
 			case "Fridge":
+				print ("entoru botao fridge");
 				GameObject.Find ("Fridge").GetComponent<Activate_button> ().enabled = true;
 				break;
 			case "Kitchen table":
 				GameObject.Find ("Kitchen table").GetComponent<Activate_button> ().enabled = true;
-				break;	
+				break;
+			case "Shower":
+				GameObject.Find ("Shower").GetComponent<Activate_button> ().enabled = true;
+				break;
+			case "Bathroom Sink":
+				break;
+			case "Kid Bedside Table":
+				break;
+			case "Desk with laptop":
+				break;
 		}
 	
 	}
+
+	Quest CheckCurrentQuest()																			//Função que diz qual é a quest atual (quest a ser apresentada na GUI)
+	{																									//Experimentar ver primeira quest da lista de quests que tenha isCompleted=false
+		int quest_length = questsList.Length;
+		int cycle_counter = 0;
+
+		while(cycle_counter<quest_length)
+		{
+			if (!questsList [cycle_counter].isCompleted) {												//Verifica a primeira quest que não foi completada. Ativa essa quest, apresenta na GUI e sai do ciclo
+				Quest CurrentQuest = questsList [cycle_counter];
+				questsList [cycle_counter].gameObject.SetActive (true);
+				QuestBox.text = "Próxima Missão:\n" + questsList[cycle_counter].questName;
+
+				return CurrentQuest;
+			}
+
+		}
+		return null;
+	}
+
+	public void StartButton()                                                       					//Iniciar o minijogo (Match 2 images)
+	{
 		
+		//Aqui provavelmente poe-se um change scene
+		//Por nos playerPrefs a info da tarefa
+		//Fazer scene load
+		//Quest quest_started = CheckCurrentQuest();
+		PlayerPrefs.SetString("Folder", /*quest_started.questName*/"TestImages");										//Tipo da Tarefa
+		SceneManager.LoadScene ("Match_minigame",LoadSceneMode.Additive);
+	}
+
 	List<string> ReadTextfile(string File_name)
 	{
 		string path = "Assets/Resources/Lists/" + File_name + ".txt";
@@ -84,6 +160,32 @@ public class QuestManager : MonoBehaviour {
 			arrayoflines.Add (line);
 		}
 		reader.Close ();
+		return arrayoflines;
+	}
+
+	List<string> AlternativeReadFile(string File_name){
+		string directory = null;
+
+		if(Application.platform == RuntimePlatform.Android)
+		{
+			print ("entrou Android");
+			directory = "Lists\\" + File_name;
+		}
+		if(Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor){
+			print ("entrou windows");
+			directory = "Lists/" + File_name;
+		}
+		print ("Pasta a aceder:" + directory);
+		TextAsset reader = Resources.Load<TextAsset>(directory); 
+
+		List<string> arrayoflines = new List<string> ();
+		print (reader.text.Length);
+		string[] linesFromfile = reader.text.Split('\n');
+		foreach (string line in linesFromfile)
+		{
+			//Debug.Log(line);
+			arrayoflines.Add (line);
+		}
 		return arrayoflines;
 	}
 }
